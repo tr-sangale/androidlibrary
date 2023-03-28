c157300c4f27a54df242bdc2e2898c6851ead9bc
Zolix.ai Core <127385111+Zolix-ai@users.noreply.github.com>
2023-03-28 14:58:00 +0100

    Test
    Pass test ( no failing )

---
+++ TestCode.java
@@ -0,0 +1,19 @@
+
+public class TestCode{
+
+    public static String buildResult(String result){
+        return result;
+    }
+
+
+public static void main(String[] args) {
+    
+    //This is CircleCLI Test Code
+
+    String message="CircleCLI Build Done!";
+    System.out.println(" " + buildResult(message));
+
+
+
+}
+}
